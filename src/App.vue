13ce4e80afa9dafeb29356a6d49d68a00b9fb27b
pmchancrin1 <pmchancrin@gmail.com>
2021-01-20 17:06:05 +0100

    add site

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="myApplication">
+    <h1>Hello World</h1>
+    <p>Petit coucou de Piermick :) </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
